6deeaf7ac24618f501bf7e534d0cb7f0f36ba327
Allan Hudgins <allan@bitaccess.co>
2023-07-31 12:27:46 -0700

    feat: renders log messages in more Memzo-oriented way

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  env: {
+    node: true
+  },
+  extends: ['standard'],
+  parserOptions: {
+    ecmaVersion: 2020
+  },
+  rules: {
+    'space-before-function-paren': 0
+  }
+}
